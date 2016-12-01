301f22c3caa8e5c6c0c483338dbb47581b6722cf
Alejandro Levinas <levinasale@gmail.com>
2016-12-01 16:15:51 -0300

    bug fix

--- src/databases/UsersDB.cpp
+++ src/databases/UsersDB.cpp
@@ -362,12 +362,7 @@ bool UsersDB::get_user_contacts(string &username, Json::Value &contacts) {
     Json::Value aux_result(Json::arrayValue);
     Json::Value user = get_user(username);
 
-    LOG(DEBUG) << "HOLAAAAAAAAAAAAA\n" << user << user["contacts"];
-
-//    std::cerr << user["contacts"];
-
     for (Json::Value& contactName: user["contacts"]) {
-//        std::string contactName = (*itr).asString();
         std::cerr << contactName;
         Json::Value contact = get_user(contactName.asString());
         aux_result.append(contact);
